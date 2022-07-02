73416e566267b1474d96858d0df81463be6ad8fd
iabdukhoshimov <i.abdukhoshimov@gmail.com>
2022-07-02 16:42:46 +0500

    added migrations

--- migrations/01_creat_table.up.sql
+++ migrations/01_creat_table.up.sql
@@ -1,1 +1,9 @@
 DROP TABLE IF EXISTS company;
+
+CREATE TABLE if not exists company(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
